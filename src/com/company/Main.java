14b6378201142aeaa3be743b41c743ce10ba0018
AnahitKhechumyan <70436905+AnahitKhechumyan@users.noreply.github.com>
2022-02-25 00:30:39 +0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
-public class Main {
+import com.company.collections.List;
+import com.company.collections.collectionsUtil.Node;
+import java.lang.reflect.Type;
+import java.util.*;
+import static javax.swing.UIManager.get;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> list = new List<>();
+
+        list.push(5);
+        list.push(8);
+        list.push(83);
+        list.remove(1);
+
+        System.out.println(list);
+        System.out.println(list.getFirst().getInfo());
+        System.out.println(list.getLast());
+
+        Stack<Integer> order = new Stack();
+
+       // List<Integer> list = new LinkedList<>();
+        list.add(5);
+        list.add(5);
+        list.add(5);
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(get(i));
+        }
+       order.push(5);
+        order.push(15);
+        order.push(8);//2
+       order.push(0);
+        order.push(7);
+        order.remove(1);
+        order.insertElementAt(0,1);
+        System.out.println(order.get(3));
+        System.out.println(order.pop());
+        System.out.println(order.size());
+       System.out.println(order);
+        System.out.println();
+       // System.out.println(order.isEmpty());
+
+       // List<Integer> list = new List<>();
+       //list.push(-1,7);
+        list.push(4,7);
+
+        list.push(5);
+        list.push(5);
+        list.push(5);
+       // list.push(10,-1);
+        list.push(1,4);
+        System.out.println(list);
     }
 }
---
+++ src/com/company/collections/List.java
@@ -0,0 +1,120 @@
+package com.company.collections;
+
+import com.company.collections.collectionsUtil.Node;
+
+public class List <Type> {
+    private Node<Type> first;
+    private  Node <Type> last;
+    private int size;
+
+    public List() {
+        first = last = null;
+        size = 0;
+    }
+
+    public Node<Type> getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node<Type> first) {
+        this.first = first;
+    }
+
+    public Node<Type> getLast() {
+        return last;
+    }
+
+    public void setLast(Node<Type> last) {
+        this.last = last;
+    }
+
+    public int size() {
+        return size;
+    }
+    public void push(Type data) {
+        if (first == null) {
+            first = last = new Node <> (data, null, null);
+        } else {
+            last.setNext(new Node<>(data, last, null));
+            last = last.getNext();
+        }
+        size++;
+    }
+    public void push(int index, Type data) {
+        if (index < 0){
+            throw new RuntimeException("index out of bound exception");
+        } else if (index > size || size == 0){
+            push(data);
+        } else if (size != 0 && index < size) {
+            Node<Type> temp = first;
+            for(int i = 0; i < index-1; i++){
+                if (temp.getNext() != null){
+                    temp = temp.getNext();
+                }
+            }
+            Node<Type> node = new Node<>(data, temp, temp.getNext());
+            temp.getNext().setPrev(node);
+            temp.setNext(node);
+            size++;
+        }
+    }
+
+    public Type pop() {
+        if (size == 0) {
+            throw new RuntimeException("massege this is error");
+        } else if (size == 1) {
+            Type temp = first.getInfo();
+            first = last = null;
+            size--;
+            return temp;
+        } else {
+            Node <Type> temp = last;
+            last = last.getPrev();
+            last.setNext(null);
+            temp.setPrev(null);
+            size--;
+            return temp.getInfo();
+        }
+    }
+    public void remove(int index) {
+        if (index < 0 || index >= size || size == 0) {
+            throw new RuntimeException("index out of bound exception");
+        } else {
+            Node<Type> temp = first;
+            for(int i = 0; i < index; i++){//2
+                if (temp.getNext() != null){
+                    temp = temp.getNext();
+                }
+            }
+            temp.getPrev().setNext(temp.getNext());
+            temp.getNext().setPrev(temp.getPrev());
+            temp.setPrev(null);
+            temp.setNext(null);
+            size--;
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("Stack{\t");
+
+        if (size == 0) {
+            s.append("no data, ");
+        } else if (size == 1) {
+            s.append("{").append(first.getInfo()).append("}, ");
+        } else {
+            Node<Type> temp = first;
+            while (temp.getPrev() != null) {
+                temp = temp.getPrev();
+            }
+            while (temp != null) {
+                s.append("{").append(temp.getInfo()).append("}, ");
+                temp = temp.getNext();
+            }
+        }
+        s.append("size=").append(size).append("\t}");
+        return s.toString();
+    }
+   public void add(Type i) {
+    }
+}
